b13ee596bc03e17fe1bde2d9b9fb83ae3a527d61
Razvan Cristea <razcristea@gmail.com>
2019-11-26 10:48:19 +0200

    built app

---
+++ docs/build/precache-manifest.aa675db4045d6beeef3aae4f26e33a2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b143bfce2abed6edb40bf165eff905b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2c1182009ff92e0e262",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "21c189ff312546eadaa3",
+    "url": "/static/js/2.e93c8410.chunk.js"
+  },
+  {
+    "revision": "f2c1182009ff92e0e262",
+    "url": "/static/js/main.0381778d.chunk.js"
+  },
+  {
+    "revision": "688c1c05c07a734cced6",
+    "url": "/static/js/runtime-main.09de0c58.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
